eabfd20ddc7a7ac9b9b6a765c2b3ca1b4964c36e
Kihold <bae3880@naver.com>
2025-01-01 21:09:49 +0900

    25.01.01

---
+++ Source/UnrealGas241226/UnrealGas241226.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealGas241226 : ModuleRules
+{
+	public UnrealGas241226(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
